38c8e01e4e69a92994f3a74b18a1e34427f0815e
hansel.tritama <hanselgunawan94@gmail.com>
2018-01-07 23:48:48 -0800

    Scraping from NYT.

---
+++ public/app.js
@@ -0,0 +1,84 @@
+function createList(data) {
+    $("#news-list").append("<div class='row' style='padding:20px; border:1px solid black'>" +
+        "<div class='col-lg-12'>" +
+        "<div class='row'>" +
+        "<div class='col-lg-9'>" +
+        "<h2>"+data.title+"</h2>" +
+        "</div>" +
+        "<div class='col-lg-3' style='margin-top: 25px'>" +
+        "<button class='btn btn-success' style='font-size: 18px'>Save Article</button>" +
+        "</div>" +
+        "</div>" +
+        "<div class='row'>" +
+        "<div class='col-lg-12'>" +
+        "<p>"+data.desc+"</p>" +
+        "</div>" +
+        "</div>" +
+        "</div>" +
+        "</div>")
+}
+
+function emptyList(){
+    $("#news-list").append("<div class='row' style='padding:20px'>" +
+        "<div class='col-lg-12' style='text-align: center'>" +
+        "<h3>No news to be shown</h3>" +
+        "</div>" +
+        "</div>");
+}
+
+// We'll be rewriting the table's data frequently, so let's make our code more DRY
+// by writing a function that takes in data (JSON) and creates a table body
+function displayNews(data) {
+  // Add to the table here...
+    if(data.length>0)
+    {
+        for(let i=0;i<data.length;i++)
+        {
+            createList(data[i]);
+        }
+    }
+    else
+    {
+        emptyList();
+    }
+}
+
+$("#myModal").modal("hide");
+
+$.getJSON("/all", function(data) {
+  // Call our function to generate a table body
+  $("#news-list").empty();
+  displayNews(data);
+});
+
+$("#scrapeBtn").on("click", function () {
+    $("#myModal").modal("show");
+    $.ajax({
+        method: "GET",
+        url: "/scrape",
+    }).done(function(data) {
+        console.log(data);
+        setTimeout(function () {
+            window.location = "/";
+        }, 1500);
+    })
+});
+
+// <div class="row" style="padding:20px; border:1px solid black">
+//     <div class="col-lg-12">
+//     </div>
+//     <div class="row">
+//     <div class="col-lg-8">
+//     <h2>HEADLINES</h2>
+//     </div>
+//     <div class="col-lg-4" style="margin-top: 25px">
+//     <button class="btn btn-success" style="font-size: 18px">Save Article</button>
+// </div>
+// </div>
+// <div class="row">
+//     <div class="col-lg-12">
+//     <p>Description about the news</p>
+// </div>
+// </div>
+// </div>
+// </div>
--- server.js
+++ server.js
@@ -1,26 +1,66 @@
 var cheerio = require("cheerio");
 var request = require("request");
+var mongojs = require("mongojs");
+var express = require("express");
 
-request("https://www.nytimes.com/section/technology?action=click&contentCollection=media&region=navbar&module=collectionsnav&pagetype=sectionfront&pgtype=sectionfront", function(error, response, html) {
+var databaseUrl = "new_york_times";
+var collections = ["news"];
 
-  var $ = cheerio.load(html);
+var app = express();
 
-  var results = [];
-  var limit = 0;
+app.use(express.static("public"));
 
-  $(".story-meta").each(function(i, element) {
+var db = mongojs(databaseUrl, collections);
 
-    var title = $(element).children(".headline").text().replace("\n", "").replace("                    ", "").replace("                ", "");
-    var desc = $(element).children(".summary").text();
+db.on("error", function(error) {
+    console.log("Database Error:", error);
+});
 
-    results.push({
-      title: title,
-      desc: desc
+app.get("/", function(req, res) {
+    res.send("Hello world");
+});
+
+app.get("/all", function(req, res) {
+    db.news.find({}, function(error, found) {
+        // Log any errors if the server encounters one
+        if (error) {
+            console.log(error);
+        }
+        // Otherwise, send the result of this query to the browser
+        else {
+            res.json(found);
+        }
     });
-    limit++;
-    if(limit === 20) return false;
-  });
+});
 
-  // Log the results once you've looped through each of the elements found with cheerio
-  console.log(results);
+app.get("/scrape", function(req, res) {
+    request("https://www.nytimes.com/section/technology?action=click&contentCollection=media&region=navbar&module=collectionsnav&pagetype=sectionfront&pgtype=sectionfront", function(error, response, html) {
+
+        var $ = cheerio.load(html);
+
+        var results = [];
+        var limit = 0;
+
+        $(".story-meta").each(function(i, element) {
+
+            var title = $(element).children(".headline").text().replace("\n", "").replace("                    ", "").replace("                ", "");
+            var desc = $(element).children(".summary").text();
+
+            db.news.insert({
+                title: title,
+                desc: desc,
+                saved: false
+            });
+            limit++;
+            if(limit === 20) return false;
+        });
+
+        // Log the results once you've looped through each of the elements found with cheerio
+        res.send("Scrape Complete");
+    });
+});
+
+// Set the app to listen on port 3000
+app.listen(3000, function() {
+    console.log("App running on port 3000!");
 });
